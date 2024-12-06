3ff3f58492f4b693fb5b669cc2682d4aafa4b151
kaizhududhdsuid <dukaizhujack@gmail.com>
2024-12-05 20:44:34 -0500

    Update coordinate_learning.py
    Documentation

--- src/embedders/coordinate_learning.py
+++ src/embedders/coordinate_learning.py
@@ -15,7 +15,7 @@ from torchtyping import TensorType
 from .metrics import distortion_loss, d_avg
 from .manifolds import ProductManifold
 
-
+"""Implementation for coordinate training and optimization"""
 def train_coords(
     pm: ProductManifold,
     dists: TensorType["n_points", "n_points"],
@@ -28,6 +28,25 @@ def train_coords(
     loss_window_size: int = 100,
     logging_interval: int = 10,
 ):
+    """
+    Coordinate training and optimization
+
+    Args:
+        pm: productManifold object that encapsulates the manifold and its signature.
+        dists: (n_points, n_points) tensor representing the pairwise distance matrix between points.
+        device: device for training (default: "cpu").
+        burn_in_learning_rate: learning rate during the burn-in phase (default: 1e-3).
+        burn_in_iterations: number of iterations during the burn-in phase (default: 2,000).
+        learning_rate: learning rate during the training phase (default: 1e-2).
+        scale_factor_learning_rate: learning rate for scale factor optimization (default: 0.0).
+        training_iterations: number of iterations for the training phase (default: 18,000).
+        loss_window_size: window size for computing the moving average of the loss (default: 100).
+        logging_interval: interval for logging the training progress (default: 10).
+
+    Returns:
+        pm.x_embed: tensor of the final learned coordinates in the manifold.
+        losses: list of loss values at each iteration during training.
+    """    
     # Move everything to the device
     pm.x_embed = pm.initialize_embeddings(n_points=len(dists)).to(device)
     dists = dists.to(device)
