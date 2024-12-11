952db1e503c8d06afc8a30b55bf92aac52e58428
Kaizhu Du <dukaizhujack@gmail.com>
2024-12-11 14:38:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="embedders",
     version="0.1",
-    packages=find_packages(where="src/embedders"),
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
     requires=[
         "torch",
