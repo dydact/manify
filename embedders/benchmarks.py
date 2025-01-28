Fix numerical issues with kappa-GCN

embedders/benchmarks.py
@@ -161,7 +161,6 @@ def benchmark(
     pdists = pm.pdist(X).detach()
 
     # Get tangent plane
-    X_tangent = pm.logmap(X).detach()
     X_train_tangent = pm.logmap(X_train).detach()
     X_test_tangent = pm.logmap(X_test).detach()
 
@@ -181,7 +180,8 @@ def benchmark(
         A_hat = get_A_hat(adj).detach()
     else:
         dists = pdists**2
-        dists /= dists.max()
+        dists_train = dists[train_idx][:, train_idx]
+        dists /= dists_train[torch.isfinite(dists_train)].max()
         A_hat = get_A_hat(dists).detach()
     A_hat = A_hat.to(device)
     A_train = A_hat[train_idx][:, train_idx]
@@ -437,7 +437,7 @@ def benchmark(
         d = X_test_stereo.shape[1] # Shape can't change between layers
         kappa_gcn = KappaGCN(pm=pm_stereo, hidden_dims=[d, d], task=task, output_dim=nn_outdim).to(device)
         t1 = time.time()
-        kappa_gcn.fit(X_train_stereo, y_train, A=A_train, **nn_train_kwargs)
+        kappa_gcn.fit(X_train_stereo, y_train, A=A_train, **nn_train_kwargs, use_tqdm=True)
         t2 = time.time()
         y_pred = kappa_gcn.predict(X_test_stereo, A=A_test)
         accs["kappa_gcn"] = _score(None, y_test_np, None, y_pred_override=y_pred, torch=True)

embedders/predictors/kappa_gcn.py
@@ -146,7 +146,7 @@ class KappaGCN(torch.nn.Module):
 
         # Hidden layers
         if hidden_dims is None:
-            dims = [pm.dim] * (n_layers + 1)
+            dims = [pm.dim, pm.dim, pm.dim] # 2 hidden layers
         elif not (all([M.curvature == 0] for M in pm.P) or all([d == pm.dim for d in hidden_dims])):
             raise ValueError("Only fully Euclidean manifolds can change hidden dimension size")
         else:
@@ -155,7 +155,7 @@ class KappaGCN(torch.nn.Module):
         self.layers = torch.nn.ModuleList([KappaGCNLayer(dims[i], dims[i+1], pm, nonlinearity) for i in range(len(dims) - 1)])
 
         # Final layer params
-        self.W_logits = torch.nn.Parameter(torch.randn(pm.dim, output_dim) * 0.01)
+        self.W_logits = torch.nn.Parameter(torch.randn(dims[-1], output_dim) * 0.01)
         self.p_ks = geoopt.ManifoldParameter(torch.zeros(output_dim, pm.dim), manifold=pm.manifold)
 
 