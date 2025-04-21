5f868002b37ab095ee9e30f4fcdb888ebf1e9518
Philippe Chlenski <pac@cs.columbia.edu>
2025-04-21 12:45:09 -0400

    Try lazy loading; consolidate pylintrc and pyproject

--- manify/__init__.py
+++ manify/__init__.py
@@ -2,4 +2,24 @@ import manify.curvature_estimation
 import manify.embedders
 import manify.manifolds
 import manify.predictors
-import manify.utils
+
+# Dynamically check for utils dependencies
+try:
+    import importlib.util
+
+    _utils_deps = ["networkx", "pandas", "matplotlib", "scipy", "anndata", "basemap"]
+    _missing_deps = [dep for dep in _utils_deps if importlib.util.find_spec(dep) is None]
+
+    if not _missing_deps:
+        import manify.utils
+
+        HAS_UTILS = True
+    else:
+        HAS_UTILS = False
+except ImportError:
+    HAS_UTILS = False
+
+# Expose flag for users to check
+__all__ = ["curvature_estimation", "embedders", "manifolds", "predictors"]
+if HAS_UTILS:
+    __all__.append("utils")
--- manify/utils/__init__.py
+++ manify/utils/__init__.py
@@ -1,5 +1,26 @@
-import manify.utils.benchmarks
-import manify.utils.dataloaders
-import manify.utils.link_prediction
-import manify.utils.preprocessing
-import manify.utils.visualization
+# Lazy loading variant of utils imports - changed to this so that missing imports would be non-breaking for core manify
+
+
+def __getattr__(name):
+    if name == "benchmarks":
+        import manify.utils.benchmarks
+
+        return manify.utils.benchmarks
+    elif name == "dataloaders":
+        import manify.utils.dataloaders
+
+        return manify.utils.dataloaders
+    elif name == "link_prediction":
+        import manify.utils.link_prediction
+
+        return manify.utils.link_prediction
+    elif name == "preprocessing":
+        import manify.utils.preprocessing
+
+        return manify.utils.preprocessing
+    elif name == "visualization":
+        import manify.utils.visualization
+
+        return manify.utils.visualization
+    else:
+        raise AttributeError(f"module 'manify.utils' has no attribute '{name}'")
