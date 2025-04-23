a1ba92ab29e9a029af6c7a9fc08b3b05937a5889
Kaizhu Du <dukaizhujack@gmail.com>
2025-04-22 22:58:29 -0400

    better format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,30 +1,12 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'manify'
 copyright = '2025, Philippe Chlenski and Kaizhu Du'
 author = 'Philippe Chlenski and Kaizhu Du'
 release = '0.0.2'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# extensions = [
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.napoleon',
-#     'sphinx_autodoc_typehints',
-#     'sphinx.ext.autosummary',
-#     'autoapi.extension',
-# ]
 extensions = [
     'sphinx.ext.napoleon',
     # 'sphinx_autodoc_typehints',
@@ -33,24 +15,28 @@ extensions = [
     # 'sphinx.ext.autosummary',
 ]
 
-# autosummary_generate = True
-# autodoc_default_options = {
-#     "members": True,
-#     "undoc-members": True,
-#     "show-inheritance": True,
-# }
-
 autoapi_type = 'python'
 autoapi_dirs = [os.path.abspath('../../manify')]
 autoapi_ignore = ["*/utils/*"]
-autodoc_typehints = "description"
+
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,  # Set to False to hide undocumented members
+    "private-members": False,
+    "show-inheritance": True,
+    "exclude-members": "__init__",
+}
+autoclass_content = "class"
+autoapi_options = [
+    "members",           # pull in documented members
+    "imported-members",  # if you want to re-export things
+    "show-inheritance",
+    # note: NO 'undoc-members' here
+]
+autodoc_typehints = "description"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+
